afb0e5fffccac9dddbe79dd4acd3e4122506c206
Andrew Stewart <andrew@stwrt.ca>
2013-12-30 14:28:27 -0800

    Add some specs for Spark

--- test/dist/specs/spark.spec.js
+++ test/dist/specs/spark.spec.js
@@ -3,7 +3,48 @@
   source('spark');
 
   describe('Cylon.Adaptors.Spark', function() {
-    return it('needs test coverage');
+    var spark;
+    spark = new Cylon.Adaptors.Spark;
+    it("exposes a 'commands' method listing Spark commands", function() {
+      var command, _i, _len, _ref, _results;
+      expect(spark.commands()).to.be.a('array');
+      _ref = spark.commands();
+      _results = [];
+      for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+        command = _ref[_i];
+        _results.push(expect(command).to.be.a('string'));
+      }
+      return _results;
+    });
+    it("exposes a digitalRead method to read from a digital pin", function() {
+      return expect(spark.digitalRead).to.be.a('function');
+    });
+    it("exposes a digitalWrite method to write to a digital pin", function() {
+      return expect(spark.digitalWrite).to.be.a('function');
+    });
+    it("exposes a analogRead method to read from a analog pin", function() {
+      return expect(spark.analogRead).to.be.a('function');
+    });
+    it("exposes a analogWrite method to write to a analog pin", function() {
+      return expect(spark.analogWrite).to.be.a('function');
+    });
+    it("exposes a pwmWrite method to write to a pwm device", function() {
+      return expect(spark.pwmWrite).to.be.a('function');
+    });
+    it("exposes a servoWrite method to write to a servo", function() {
+      return expect(spark.servoWrite).to.be.a('function');
+    });
+    return describe('#pinVal', function() {
+      it("returns 'HIGH' if the value is 1", function() {
+        return expect(spark.pinVal(1)).to.be.eql("HIGH");
+      });
+      it("returns 'LOW' if the value is 0", function() {
+        return expect(spark.pinVal(0)).to.be.eql("LOW");
+      });
+      return it("returns 'LOW' for all other values", function() {
+        return expect(spark.pinVal("invalid")).to.be.eql("LOW");
+      });
+    });
   });
 
 }).call(this);
--- test/src/specs/spark.spec.coffee
+++ test/src/specs/spark.spec.coffee
@@ -1,6 +1,39 @@
-'use strict';
+'use strict'
 
 source 'spark'
 
 describe 'Cylon.Adaptors.Spark', ->
-  it 'needs test coverage'
+  spark = new Cylon.Adaptors.Spark
+
+  it "exposes a 'commands' method listing Spark commands", ->
+    expect(spark.commands()).to.be.a 'array'
+    for command in spark.commands()
+      expect(command).to.be.a 'string'
+
+  it "exposes a digitalRead method to read from a digital pin", ->
+    expect(spark.digitalRead).to.be.a 'function'
+
+  it "exposes a digitalWrite method to write to a digital pin", ->
+    expect(spark.digitalWrite).to.be.a 'function'
+
+  it "exposes a analogRead method to read from a analog pin", ->
+    expect(spark.analogRead).to.be.a 'function'
+
+  it "exposes a analogWrite method to write to a analog pin", ->
+    expect(spark.analogWrite).to.be.a 'function'
+
+  it "exposes a pwmWrite method to write to a pwm device", ->
+    expect(spark.pwmWrite).to.be.a 'function'
+
+  it "exposes a servoWrite method to write to a servo", ->
+    expect(spark.servoWrite).to.be.a 'function'
+
+  describe '#pinVal', ->
+    it "returns 'HIGH' if the value is 1", ->
+      expect(spark.pinVal(1)).to.be.eql "HIGH"
+
+    it "returns 'LOW' if the value is 0", ->
+      expect(spark.pinVal(0)).to.be.eql "LOW"
+
+    it "returns 'LOW' for all other values", ->
+      expect(spark.pinVal("invalid")).to.be.eql "LOW"
