4035e801d8dfc5086629c2077adf530e8a5d9116
deadprogram <ron@hybridgroup.com>
2014-05-23 16:58:39 -0700

    Should be working

---
+++ examples/voodooblink/voodooblink.js
@@ -0,0 +1,20 @@
+var Cylon = require('cylon');
+
+Cylon.robot({
+  connection: {
+    name: 'voodoospark',
+    adaptor: 'voodoospark',
+    accessToken: '[YOUR_ACCESS_TOKEN]',
+    deviceId: '[YOUR_DEVICE_ID]'
+  },
+
+  device: {
+    name: 'led',
+    driver: 'led',
+    pin: 'D7'
+  },
+
+  work: function(my) {
+    every((1).second(), function() {my.led.toggle()});
+  }
+}).start();
